35238750bb4be59990eb22e1a15fb308b69efe0b
Nickolai Ernst <nickolai.lundby.ernst1@post.au.dk>
2018-03-20 19:04:18 +0100

    Outline. Failing tests

--- Microwave.Test.Integration/IT1_Button_Door.cs
+++ Microwave.Test.Integration/IT1_Button_Door.cs
@@ -8,51 +8,72 @@ using NSubstitute;
 using NUnit.Framework;
 using MicrowaveOvenClasses.Controllers;
 using MicrowaveOvenClasses.Interfaces;
+using MicrowaveOvenClasses.Boundary;
 using Timer = MicrowaveOvenClasses.Boundary.Timer;
 
 namespace Microwave.Test.Integration
 {
 	[TestFixture]
-	class IT1_Button_Door
+	public class IT1_Button_Door
 	{
-		private IUserInterface _userInterface;
+        // Substitutes
+        private IDisplay _display;
+	    private ILight _light;
+	    private ITimer _timer;
+	    private IPowerTube _powerTube;
+	    private ICookController _cookController;
+	    private IUserInterface _userInterface;
 
-		private IButton _powerButton;
-		private IButton _timeButton;
-		private IButton _startCancelButton;
+        // Units under test
+        private IButton _uut_powerButton;
+		private IButton _uut_timeButton;
+		private IButton _uut_startCancelButton;
+		private IDoor _uut_door;
 
-		private IDoor _door;
-
-		private IDisplay _display;
-		private ILight _light;
-
-		private CookController _cookController;
-
-		private ITimer _timer;
-		private IPowerTube _powerTube;
-
-		[SetUp]
+        [SetUp]
 		public void SetUp()
 		{
-			_powerButton = Substitute.For<IButton>();
-			_timeButton = Substitute.For<IButton>();
-			_startCancelButton = Substitute.For<IButton>();
-
-			_door = Substitute.For<IDoor>();
-
+            // Substitute setup
 			_display = Substitute.For<IDisplay>();
 			_light = Substitute.For<ILight>();
-
-			_timer = new IT4_Timer();
-
+		    _timer = Substitute.For<ITimer>();
 			_powerTube = Substitute.For<IPowerTube>();
+		    _cookController = Substitute.For<ICookController>();
+		    _userInterface = Substitute.For<IUserInterface>();
+			// _cookController.UI = _userInterface;
 
-			_cookController = new CookController(_timer, _display, _powerTube);
-			_userInterface = new UserInterface(_powerButton, _timeButton, _startCancelButton, _door, _display, _light, _cookController);
-			_cookController.UI = _userInterface;
-		}
+            // Uut setup
+		    _uut_powerButton = new Button();
+		    _uut_timeButton = new Button();
+		    _uut_startCancelButton = new Button();
+		    _uut_door = new Door();
+        }
 
-		[Test]
+	    [Test]
+	    public void Door_Open_UserInterfaceIsReady_LightOn()
+	    {
+	        _uut_door.Open();
+	        _light.Received().TurnOn();
+	    }
 
+	    [Test]
+	    public void Door_Close_DoorIsOpen_LightOff()
+	    {
+	        _uut_door.Open();
+	        _uut_door.Close();
+	        _light.Received().TurnOff();
+	    }
+
+	    public void Door_Open_IsCooking_CookingStops()
+	    {
+	        _uut_powerButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+	        _uut_timeButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+	        _uut_startCancelButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+
+	        _uut_door.Open();
+	        _cookController.Received().Stop();
+	        _display.Received().Clear();
+	        _light.Received().TurnOff();
+	    }
 	}
 }
